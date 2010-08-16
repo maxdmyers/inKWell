272e6cba8344a91fe9a632d2ea596449fbbe922b
Matthew J. Sahagian <msahagian@dotink.org>
2010-08-15 20:10:25 -0700

    - Another changed from ASP Tags via inkwell_site that wasn't pulled in in the patch

--- user/views/html.php
+++ user/views/html.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 	<head>
-		<title><?= $this->rcombine('title') ?></title>
-		<? $this->compress('styles');  ?>
-		<? $this->compress('scripts'); ?>
+		<title><%= $this->rcombine('title') %></title>
+		<% $this->compress('styles');  %>
+		<% $this->compress('scripts'); %>
 	</head>
-	<body id="<?= $this->pull('id') ?>" class="<?= $this->combine('classes', ' ') ?>">
-		<? $this->place('header'); ?>
+	<body id="<%= $this->pull('id') %>" class="<%= $this->combine('classes', ' ') %>">
+		<% $this->place('header'); %>
 		<div class="torso">
-			<? $this->place('primary_section')   ?>
-			<? $this->place('secondary_section') ?>
-			<? $this->place('tertiary_section')  ?>
+			<% $this->place('primary_section')   %>
+			<% $this->place('secondary_section') %>
+			<% $this->place('tertiary_section')  %>
 		</div>
-		<? $this->place('footer'); ?>
+		<% $this->place('footer'); %>
 	</body>
 </html>
