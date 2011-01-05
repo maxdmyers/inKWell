3f5a563dc9764125513a6a3b24596382c071919f
Matthew J. Sahagian <msahagian@dotink.org>
2011-01-05 07:00:00 -0800

    - Comment changes in config files

--- includes/config/autoloaders.php
+++ includes/config/autoloaders.php
@@ -1,26 +1,36 @@
 <?php
 
 	// The inKWell autoloader works via a series of match parameters and
-	// targets.
+	// target directories configured in a key => value array respectively.
 	//
-	// Match parameters can be standard strings, strings with wildcards, or
-	// class names.  If the string contains a * they are used as wildcards, so
-	// for example the following would make any class beginning with
-	// 'user' load from the 'includes/lib/user' directory:
+	// Match parameters can be any one of the following:
+	//   * Strings with wildcards
+	//   * Standard strings
+	//   * Class names
+	//
+	// If the key contains a * they are used as wildcards, so for example the
+	// following would make any class beginning with 'user' load from the
+	// 'includes/lib/user' directory:
 	//
 	//      'user*'  =>   'includes/lib/user'
 	//
-	// In the event a string does not contain any wildcards the autoloader
-	// will try to determine whether or not it is a class.  If it is a class
-	// it will run the magic method defined by iw::MATCH_CLASS_METHOD.  This
-	// method is completely customizable, but is expected to to return TRUE or
-	// FALSE based on it's custom match logic.  If the method returns TRUE
-	// the autoloader will attempt to load the class from the target.
+	// Any class which does not begin with 'user' will have no attempt made to
+	// load from the 'includes/lib/user' directory.
 	//
-	// Finally, if the match parameter contains no wilcards, is not a class or
-	// does not have a match method set on the class, it will be treated as
-	// a static match parameter, and try to load from the target directory
-	// regardless.
+	// Non-wildcard keys represent either static matches, i.e. no matter what
+	// the class name an attempt will be made to load from that directory,
+	// or class names.
+	//
+	// In the event that the key represents a class name, the requested class
+	// name will be passed to the __match() method on the said class for custom
+	// matching logic.  If the __match() method returns TRUE an attempt will be
+	// made to load the class from that directory, if it returns FALSE, no
+	// attempt will be made.
+	//
+	// Finally, if the key contains no wilcards or does not represent a class
+	// with a __match() method, the entry will be treated as a static match,
+	// and an attempt will be made to load the class from the target directory
+	// regardless of the name.
 	//
 	// In ALL cases, if the class cannot be loaded from a target directory
 	// the autoloader will move on to the next match => target pair.
@@ -31,19 +41,23 @@
 
 	return iw::createConfig('Global', array(
 
-		// We use flourish for tons of stuff, including init, so it comes first
+		// Note: All paths are relative to document root
+		//
+		// Our base libraries, Flourish and Moor use the wildcard based keys for
+		// class matching.  In short, this means that attempts to load classes
+		// which do not match the provided prefixes will skip these directories.
 
 		'f*'           => 'includes/lib/flourish/classes',
-
-		// Moor is our router, so that comes second
-
 		'Moor*'        => 'includes/lib/moor',
 
-		// Our Core Library
+		// A non-wilcard string which does not represent a class means that an
+		// attempt will be made to load any class from this directory.
 
 		'library'      => 'includes/lib',
 
-		// Then we have our Controllers, Models, RecordSets
+		// Our base library classes have __match() methods designed to
+		// determine whether or not the class name matches a potential child
+		// class.
 
 		'Controller'   => 'user/controllers',
 		'ActiveRecord' => 'user/models',
--- includes/config/lib/view.php
+++ includes/config/lib/view.php
@@ -2,15 +2,28 @@
 
 	return iw::createConfig('Library', array(
 
+		// The directory relative to $_SERVER['DOCUMENT_ROOT'] in which views
+		// are stored.  Using the set() or add() method on a view will prepend
+		// this directory.
+		//
+		// Example :
+		// Code    : $controller->view->add('content', 'pages/home.html')
+		// Loads   : $_SERVER['DOCUMENT_ROOT']/view_root/pages/home.html
+
 		'view_root'           => 'user/views',
 
-		// A per Flourish's code minification, we can set minification modes of
-		// either 'developer' or 'production'
+		// As per Flourish's code minification, we can set minification modes
+		// of either 'developer' or 'production' -- The differences between
+		// the two relate to caching and are outlined under the Minification
+		// section at: http://flourishlib.com/docs/fTemplating
+		//
+		// If set to NULL, no minification will take place at all.
 
 		'minification_mode'   => 'development',
 
-		// Please note that this will be relative to the inKWell write
-		// directory.
+		// The cache directory is relative to the global write directory, 
+		// default $_SERVER['DOCUMENT_ROOT']/writable and is used to store
+		// cached versions of minified Javascript and CSS.
 
 		'cache_directory'     => 'cache'
 	));
