3ba6e0b7d22209a7bc3182954ad2966aec4ec72c
Matthew J. Sahagian <msahagian@dotink.org>
2011-01-08 16:53:29 -0800

    - Removed custom error handlers from controller

--- includes/config/lib/controller.php
+++ includes/config/lib/controller.php
@@ -29,19 +29,19 @@
 		'errors'                 => array(
 
 			'not_found'          => array(
-				'handler'        => 'PagesController::notFound',
+				'handler'        => NULL,
 				'header'         => 'HTTP/1.0 404 Not Found',
 				'message'        => 'The requested resource could not be found'
 			),
 
 			'not_authorized'     => array(
-				'handler'        => 'AuthController::login',
+				'handler'        => NULL,
 				'header'         => 'HTTP/1.0 401 Not Authorized',
 				'message'        => 'The requested resource requires authorization'
 			),
 
 			'forbidden'          => array(
-				'handler'        => 'PagesController::forbidden',
+				'handler'        => NULL,
 				'header'         => 'HTTP/1.0 403 Forbidden',
 				'message'        => 'You do not have permission to view the requested resource'
 			)
