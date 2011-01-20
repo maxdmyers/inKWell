fcaefdc3a53411e5ed26ff615779df4fe69969f1
Matthew J. Sahagian <msahagian@dotink.org>
2011-01-20 04:56:28 -0800

    - Modularized routes

---
+++ includes/config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+	/**
+	 * Below is a list of routes which Moor (the router) will try to resolve
+	 * when the /includes/routing.php file is called.  In addition to the
+	 * below routes, any controller configurations containing a 'routes' key
+	 * will have it's routes run as well in the order of its 'routing_priority.'
+	 * The routing priority of this file is 0, meaning all of these routes are
+	 * checked prior to the routes found in Controller configurations.
+	 */
+
+	return iw::createConfig('Core', array(
+
+
+	));
--- includes/routing.php
+++ includes/routing.php
@@ -1,9 +1,73 @@
 <?php
 
-	Moor::setRequestParamPattern('[A-Za-z0-9_-]+');
+	$router = Moor::setRequestParamPattern('[A-Za-z0-9_-]+');
 
-	Moor::
+	// TODO: check for cached routes with specificity and load those
 
-		route('/server_information', 'phpinfo') ->
+	// Register all of our custom routes first
 
-	run();
+	foreach (iw::getConfig('routes') as $route => $target) {
+		Moor::route($route, $target);
+	}
+
+	// Look for controller configurations which specify routes and sort them
+	// by a specificity and then register them in the order of highest to
+	// leaste specific.
+
+	$controller_configs = iw::getConfigsByType('Controller');
+	$routes             = array();
+	$ordered_routes     = array();
+
+	foreach ($controller_configs as $controller_config) {
+
+		if (isset($controller_config['routes'])) {
+
+			if (!is_array($controller_config['routes'])) {
+				throw new fProgrammerException(
+					'Wrong data type for routes configuration.'
+				);
+			}
+
+			foreach ($controller_config['routes'] as $route => $target) {
+
+				$route_parts = explode('/', $route);
+				$route_depth = count($route_parts);
+				$specificity = $route_depth * 10;
+
+				foreach ($route_parts as $route_part) {
+
+					switch ($route_part[0]) {
+						case ':':
+							$extra_points = 1;
+							break;
+						case '@':
+							$extra_points = 2;
+							break;
+						default:
+							$extra_points = strlen($route_part);
+							break;
+					}
+
+					$specificity += $extra_points;
+				}
+
+				$routes[$route] = array(
+					'target'      => $target,
+					'specificity' => $specificity
+				);
+
+			}
+		}
+	}
+
+	foreach($routes as $index => $route) {
+		$ordered_routes[$index] = strtolower($route[$specificity]);
+	}
+
+	arsort($ordered_routes);
+
+	foreach ($ordered_routes as $index => $specificity) {
+		Moor::route($index, $routes[$index]['target']);
+	}
+
+	$router->run();
