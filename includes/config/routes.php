0a2cceb3021d27a2657a065e8a355ff5b3f0dd87
Matthew J. Sahagian <msahagian@dotink.org>
2011-02-08 23:36:35 -0800

    - Comment update

--- includes/config/routes.php
+++ includes/config/routes.php
@@ -4,9 +4,8 @@
 	 * Below is a list of routes which Moor (the router) will try to resolve
 	 * when the /includes/routing.php file is called.  In addition to the
 	 * below routes, any controller configurations containing a 'routes' key
-	 * will have it's routes run as well in the order of its 'routing_priority.'
-	 * The routing priority of this file is 0, meaning all of these routes are
-	 * checked prior to the routes found in Controller configurations.
+	 * will have its routes run as well in the order of general route
+	 * specificity.
 	 */
 
 	return iw::createConfig('Core', array(
