fdf4f811bd7288b384996b38c4b0756d0dafdbb8
Matthew J. Sahagian <msahagian@dotink.org>
2011-03-19 14:05:08 -0700

    - Implemented most of scalePercent and prepare methods. - Added a serious amount of error checking. - Renamed :pkey to :slug.

--- includes/config/controllers/dynamic_image_controller.php
+++ includes/config/controllers/dynamic_image_controller.php
@@ -1,17 +1,23 @@
 <?php
 
 	return iw::createConfig('Controller', array(
-	
+
 		// Which directory, relative to the writable folder, to use for caching
 		// images.
 
 		'cache_directory' => 'cache/images',
-	
+
+		// An array of valid output formats, default is jpg and png only
+
+		'valid_formats'   => array(
+			'jpg', 'png'
+		),
+
 		// Routes which allow for accessing the image via URL
-	
+
 		'routes' => array(
-			'/images/:entry/:pkey/:column/width/:width/:name.:format'   => 'DynamicImagesController::scale',
-			'/images/:entry/:pkey/:column/height/:height/:name.:format' => 'DynamicImagesController::scale',
-			'/images/:entry/:pkey/:column/scale/:percent/:name.:format' => 'DynamicImagesController::scalePercent'
+			'/images/:entry/:slug/:column/width/:width/:name.:request_format'   => 'DynamicImagesController::scale',
+			'/images/:entry/:slug/:column/height/:height/:name.:request_format' => 'DynamicImagesController::scale',
+			'/images/:entry/:slug/:column/scale/:percent/:name.:request_format' => 'DynamicImagesController::scalePercent'
 		)
 	));
--- user/controllers/DynamicImagesController.php
+++ user/controllers/DynamicImagesController.php
@@ -14,23 +14,72 @@
 	 */
 	class DynamicImagesController extends Controller
 	{
-		private $imageId   = NULL;
-		private $format    = NULL;
-		private $image     = NULL;
-		private $cacheFile = NULL;
-		
 		const DEFAULT_CACHE_DIRECTORY = 'cache/images';
 
+		private $cached = FALSE;
+
+		/**
+		 * The root cache directory
+		 *
+		 * @static
+		 * @access private
+		 * @var string|fDirectory
+		 */
 		static private $cacheDirectory = NULL;
+
+		/**
+		 * An array of valid request formats
+		 *
+		 * @static
+		 * @access private
+		 * @var array
+		 */
+		static private $validFormats = NULL;
 
 		/**
 		 * Prepares a new DynamicImagesController for running actions.
 		 *
-		 * @param string void
+		 * @access protected
+		 * @param string $entry
+		 * @param string $slug
+		 * @param string $column
+		 * @param string $cache_dir
 		 * @return void
 		 */
-		protected function prepare()
+		protected function prepare($entry, $slug, $column, $format)
 		{
+			if (!$entry || !$slug || !$column || !$format) {
+				throw new fProgrammerException(
+					'You must provide at least an entry, slug, and column.'
+				);
+			}
+
+			if (!in_array($format, self::$validFormats)) {
+				throw new fProgrammerException(
+					'The requested format is not a valid output format.'
+				);
+			}
+
+			$record_class = ActiveRecord::classFromEntry($entry);
+			$record       = ActiveRecord::createFromSlug($slug);
+			$cache_path   = implode(DIRECTORY_SEPARATOR, array(
+				self::$cacheDirectory,
+				$entry,
+				fGrammar::pluralize($column),
+				iw::makeTarget($slug, md5(fURL::get()))
+			));
+
+			try {
+				$this->view   = new fImage($cache_path);
+				$this->cached = TRUE;
+			} catch (fException $e) {
+				$method     = 'get' . fGrammar::camelize($column, TRUE);
+
+				//TODO: should be loading up and copying the image to the cache
+				//TODO: path, and returning that one.
+
+				$this->view = new fImage($record->$method());
+			}
 		}
 
 		/**
@@ -48,6 +97,10 @@
 				? iw::getWriteDirectory($config['cache_directory'])
 				: iw::getWriteDirectory(self::DEFAULT_CACHE_DIRECTORY);
 
+			self::$validFormats   = (isset($config['valid_formats']))
+				? $config['valid_formats']
+				: array('jpg', 'png');
+
 			return TRUE;
 		}
 
@@ -56,25 +109,77 @@
 		 *
 		 * @static
 		 * @access public
+		 * @param string $entry The entry name for the record, ex. users
+		 * @param string $slug The string representation of the pkey, e.g. 1
+		 * @param string $column The name of the column, ex. portrait
 		 * @param integer $width The width to scale to
 		 * @param integer $height The height to scale to
 		 * @return fImage The fImage object representing the image
 		 */
-		static public function scale($width = NULL, $height = NULL)
+		static public function scale($entry = NULL, $slug = NULL, $column = NULL, $width = NULL, $height = NULL, $format = NULL)
 		{
-		
+			$entry  = fRequest::get('entry', 'string', $entry);
+			$slug   = fRequest::get('slug', 'string', $slug);
+			$column = fRequest::get('column', 'string', $column);
+			$width  = fRequest::get('width', 'string', $width);
+			$height = fRequest::get('height', 'string', $height);
+			$format = ($format)
+				? $format
+				: self::getRequestFormat();
+
 		}
-		
+
 		/**
 		 * Scale an image using a percentage
 		 *
 		 * @static
 		 * @access public
+		 * @param string $entry The entry name for the record, ex. users
+		 * @param string $slug The string representation of the pkey, e.g. 1
+		 * @param string $column The name of the column, ex. portrait
 		 * @param mixed $percent The percent of the original size to scale to
 		 * @return fImage The fImage object representing the image
 		 */
-		static public function scalePercent($percent = NULL)
+		static public function scalePercent($entry = NULL, $slug = NULL, $column = NULL, $percent = NULL, $format = NULL)
 		{
-		
+			$entry   = fRequest::get('entry', 'string', $entry);
+			$slug    = fRequest::get('slug', 'string', $slug);
+			$column  = fRequest::get('column', 'string', $column);
+			$percent = fRequest::get('percent', 'string', $percent);
+			$format  = ($format)
+				? $format
+				: self::getRequestFormat();
+
+			$percent = ($percent < 1)
+				? $percent * 100
+				: $percent;
+
+			if ($percent > 100) {
+				if (self::checkEntryAction(__CLASS__, __FUNCTION__)) {
+					self::triggerError('not_found');
+				} else {
+					throw new fProgrammerException(
+						'Upscaling images is not allowed.'
+					);
+				}
+			}
+
+			self::validateFormat($format);
+
+			try {
+				$image = new self($entry, $slug, $column, $format);
+			} catch (fException $e) {
+				if (self::checkEntry(__CLASS__)) {
+					self::trigger('not_found');
+				} else {
+					throw $e;
+				}
+			}
+
+			if (!$image->cached) {
+				//TODO: Resize the image accordingly and save it
+			}
+
+			//TODO: Output the image
 		}
 	}
