90b28a39f7b127848fcbc0cba28dcd95c380f9da
Matthew J. Sahagian <gent@yerevan.(none)>
2011-05-12 13:09:46 -0700

    - Moved inkwell interface to separate file

---
+++ includes/lib/interfaces/inkwell.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * The inKWell interface is used to determine whether or not a class will
+	 * support the following methods:
+	 *
+	 *  __init()
+	 *  __match()
+	 *  __build()
+	 *
+	 * It does not actually provide any required methods, but is only used to
+	 * suggest to the framework that support may be available.
+	 *
+	 * @author Matthew J. Sahagian [mjs] <gent@dotink.org>
+	 * @copyright Copyright (c) 2011, Matthew J. Sahagian
+	 * @license http://www.gnu.org/licenses/agpl.html GNU Affero General Public License
+	 *
+	 * @package inKWell
+	 */
+	interface inkwell {
+
+	}
