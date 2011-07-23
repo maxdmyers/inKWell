14c9095e65e45064c802c0dc890cb05526e1956a
Matthew J. Sahagian <msahagian@dotink.org>
2011-07-23 14:05:31 -0700

    - Removed __rewrite (I Think):
    The PATH_TRANSLATED variable is only supposed to exist if rewrites have been done in some way shape or form.  Or so is my understanding.  The index "front controller" has been modified to adjust variables based on this instead of the old __rewrite param.  This is not tested but should work.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 	$include_directory = 'includes';
 
-	// Check for rewrite if it's not set, prep some $_SERVER variables
+	// 
 
-	if (!isset($_GET['__rewrite'])) {
+	if (!isset($_SERVER['PATH_TRANSLATED'])) {
 		if ($_SERVER['REQUEST_URI'] == '/' || empty($_SERVER['REQUEST_URI'])) {
 			$_SERVER['PATH_INFO']   = '/';
 			$_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'];
