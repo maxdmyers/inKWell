7b325ff381ca297e08fd7ec864bc41b5a9b2fc3d
Matthew J. Sahagian <msahagian@dotink.org>
2011-12-23 02:26:27 -0800

    Fixed up console issues with reset.  Removed xdebug stuff from console wrappers and htaccess and user.ini (too unique and not by default with PHP so irrelevant really)

--- iw.console.bat
+++ iw.console.bat
@@ -1,2 +1,2 @@
 @echo off
-php -q -d register_globals=0 -d magic_quotes_gpc=0 -d short_open_tag=0 -d asp_tags=1 -d xdebug.profiler_enabler=0 -d display_errors=1 .console %1
+php -q -d register_globals=0 -d magic_quotes_gpc=0 -d short_open_tag=0 -d asp_tags=1 -d display_errors=1 .console %1
