68816d890c8a02581aa37b102513058aa04942dc
Matthew J. Sahagian <msahagian@dotink.org>
2012-02-22 09:11:40 -0800

    Removed legacy views for things better handled in codes.  Completely moved script/styles to assets

--- user/views/json.php
+++
@@ -1,6 +0,0 @@
-{
-	"id"       : <%= fJSON::encode($this->pull('id')) %>,
-	"title"    : <%= JFSON::encode($this->rcombine('title')) %>,
-	"classes"  : <%= fJSON::encode($this->pull('classes', array())) %>,
-	"contents" : <% $this->place('contents'); %>
-}
--- user/views/xml.php
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<response id="<%= fXML::encode($this->pull('id')) %>" classes="<%= fXML::encode($this->combine('classes', ' ')) %>">
-	<%= fXML::encode($this->place('contents')) %>
-</response>
