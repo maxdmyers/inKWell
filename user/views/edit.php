83a60294cda6608f71addaab9db8528dec8c6fcc
Matthew J. Sahagian <msahagian@dotink.org>
2012-04-10 01:26:14 -0700

    ...

--- user/views/edit.php
+++ user/views/edit.php
@@ -3,5 +3,5 @@
 <form action="" method="post">
 	<textarea rows="20" cols="100"><%= fHTML::encode($this->pull('source')) %></textarea>
 	<button type="submit" name="action" value="save">Save</button>
-	<button type="submit" name="action" value="preview">Save</button>
+	<button type="submit" name="action" value="preview">Preview</button>
 </form>
