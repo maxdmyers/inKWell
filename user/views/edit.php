e0a029e955e9404fad9ed6c819f0a1745d28d7cd
Matthew J. Sahagian <msahagian@dotink.org>
2012-04-10 01:44:30 -0700

    ...

--- user/views/edit.php
+++ user/views/edit.php
@@ -1,7 +1,7 @@
 <h3>Create or Edit the Page @ <%= fHTML::encode(fURL::get()) %></h3>
 
 <form action="" method="post">
-	<textarea rows="20" cols="100"><%= fHTML::encode($this->pull('source')) %></textarea>
+	<textarea rows="20" cols="100" name="source"><%= fHTML::encode($this->pull('source')) %></textarea>
 	<button type="submit" name="action" value="save">Save</button>
 	<button type="submit" name="action" value="preview">Preview</button>
 </form>
