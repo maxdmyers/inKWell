b19e394a20f6e69966c69b3aec6173d631ceeaec
Matthew J. Sahagian <msahagian@dotink.org>
2012-04-14 10:35:33 -0700

    ...

--- user/views/default.php
+++ user/views/default.php
@@ -4,6 +4,18 @@
 		<title>inKWell - A PHP MVC Framework for PHP Developers</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-30892540-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
+
 		<script src="http://yandex.st/highlightjs/6.1/highlight.min.js" type="text/javascript"></script>
 		<script type="text/javascript">
 			hljs.initHighlightingOnLoad();
