31b70db315ee82008495fc546f8a544b1a019ed9
Matthew J. Sahagian <msahagian@dotink.org>
2012-05-13 19:09:44 -0700

    Made PHP_CMD and RLWRAP_CMD variables in the console wrappers.  This makes it easier to explicitly add a path if needed.

--- iw.console.bat
+++ iw.console.bat
@@ -1,2 +1,3 @@
 @echo off
-php -q -d register_globals=0 -d magic_quotes_gpc=0 -d short_open_tag=0 -d asp_tags=1 -d display_errors=1 .console %1
+set PHP_CMD=php
+%PHP_CMD% -q -d register_globals=0 -d magic_quotes_gpc=0 -d short_open_tag=0 -d asp_tags=1 -d display_errors=1 .console %1
